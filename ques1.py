39373c69c0636f69fc158bb3d3156a7122625266
Shobhandeb Paul <devshobhan4@gmail.com>
2024-05-20 20:49:54 +0530

    python q-1

---
+++ ques1.py
@@ -0,0 +1,17 @@
+#User will input (3ages).Find the oldest one
+
+#using max() built-in function
+
+age1 = input('Enter the age1:')
+age2 = input('Enter the age2:')
+age3 = input('Enter the age3:')
+
+oldest = max(age1,age2,age3)
+print("The oldest one is:",oldest)
+
+youngest = min(age1,age2,age3)
+print("The oldest one is:",youngest)
+
+
+
+
