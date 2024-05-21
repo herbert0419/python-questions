8889a7e58f40ee576af3f9ea89995676bccde8fa
Shobhandeb Paul <devshobhan4@gmail.com>
2024-05-21 20:39:31 +0530

    c-f

---
+++ ques2.py
@@ -0,0 +1,7 @@
+#Write a program that will convert celsius value to fahrenheit
+
+#Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32
+
+celcius  = int(input("Enter Temperature in C:"))
+farhenheit = (celcius * 9/5) + 32
+print("The celcius to farhenheit conversion value is",farhenheit)
